04fcf4f5dd8245c288918fded0630cd5e1c80b15
Albert Li <libo0001@gmail.com>
2014-10-16 21:56:18 -0400

    exhaustively generate all sequences

---
+++ src/edu/gatech/eilab/scheherazade/generation/ExhaustiveSeqGen.scala
@@ -0,0 +1,54 @@
+package edu.gatech.eilab.scheherazade
+
+import main._
+import data._
+import graph._
+import io._
+import java.io._
+import graph.passage._
+import scala.collection.mutable.Queue
+import edu.gatech.eilab.scheherazade.graph.sample.CfRSample
+
+package generation {
+  /**
+   * exhaustively generate all sequences from a plot graph
+   * We store everything in the memory, which requires the number of sequences to be small.
+   */
+  object ExhaustiveSeqGen {
+
+    def generate(graph: Graph): List[List[Cluster]] =
+      {
+        var allSeq = List[List[Cluster]]()
+        var firstWalk: AbstractPassage = Passage.init(graph)
+        var queue = Queue[(AbstractPassage, List[Cluster])]()
+        queue.enqueue((firstWalk, List[Cluster]()))
+        while (!queue.isEmpty) {
+          val top = queue.dequeue
+          val walk = top._1
+          val history = top._2
+          if (walk.hasMoreSteps) {
+            val fringe = walk.fringe
+            for (step <- fringe) {
+              val newWalk = walk.forward(step)
+              val newHistory = step :: history
+              queue.enqueue((newWalk, newHistory))
+            }
+          } else {
+            allSeq = history.reverse :: allSeq
+          }
+        }
+
+        allSeq
+      }
+    
+  def main(args: Array[String]) {
+    val graph = CfRSample.graph9
+    val all = generate(graph)
+    println(all.map(_.map(_.name).mkString(", ")).mkString("\n"))
+  }
+  
+  
+  }
+  
+  
+}
